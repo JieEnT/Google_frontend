32d902314c5745bd14725b80fb1fcc04af3204de
yuenkm40 <yuenkm40@gmail.com>
2022-05-23 00:01:13 +0800

    added progress bar for nursery

--- src/components/Dashboard/Nursery.jsx
+++ src/components/Dashboard/Nursery.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import EmptyNursery from './empty_nursery.jpg'
+import Progress from './Progress'
+
 const Container = styled.div`
     margin:25px 100px;
     padding: 20px;
@@ -11,33 +13,52 @@ const Container = styled.div`
     border-radius:10px;
     display:flex;
     align-items:center;
-    justify-content:center;
+    justify-content:left;
 `;
 
 const AboutContainer = styled.div`
     display:flex;
     flex-direction:column;
     align-items:center;
-    justify-content:center;
+    justify-content:left;
 `;
-// const Title = styled.h1`
-//   font-size:15px;
-//   color:darkgrey;
-//   text-align:left;
-// `;
+const Title = styled.h1`
+  font-size:15px;
+  color:black;
+  text-align:left;
+  margin:15px;
+`;
 
 const Image = styled.img`
   height:300px;
   position:relative;
-  margin:5px 50px;
+  margin:15px 50px;
 `;
 
 const Nursery = () => {
+    const [value,setValue] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setValue (oldValue => {
+                const newValue = oldValue + 10;
+
+                if (newValue === 100) {
+                    clearInterval(interval);
+                }
+                return newValue;
+            });
+        },1000);
+    },[]);
+
   return (
     <Container>
     <AboutContainer>
     <Image src= {EmptyNursery}></Image>
-
+    </AboutContainer>
+    <AboutContainer>
+    <Title>To next stage of agricultural cycle</Title>
+    <Progress color={"darkblue"} width={"300px"}value={value} max={100}/>
+    <Title>Vegetables in garden: Kale</Title>
     </AboutContainer>
     </Container>
   )
