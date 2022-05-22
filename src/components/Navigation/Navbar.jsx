effd9896f0a0e677f229a0ca396e2936263fb30f
JieEnT <jieen1000@gmail.com>
2022-05-23 06:46:50 +0800

    Added logout button

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { Link } from 'react-router-dom';
 
 import styled from 'styled-components'
 import NavLinks from './NavLinks';
 import MainHeader from './MainHeader';
 import Login from '../Login';
+import Logout from '../Logout';
+import {AuthContext} from "../../context/auth-context";
 
 import './Navbar.css';
 
@@ -26,6 +28,8 @@ const MenuItem = styled.li`
 `;
 
 const NavBar = () => {
+    const auth = useContext(AuthContext);
+
     return (
     <MainHeader>
             <h1 className="main-navigation__title">
@@ -35,7 +39,8 @@ const NavBar = () => {
                 {/* Navlinks renders the different tabs */}
                 <NavLinks/>
             </Menu>
-            <Login/>
+            {!auth.isLoggedIn && <Login/>}
+            {auth.isLoggedIn && <Logout/>}
     </MainHeader>
     );
 };
