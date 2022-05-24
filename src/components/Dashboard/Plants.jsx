97c6936550ab3f154881288a9f16ef59c132033b
yuenkm40 <yuenkm40@gmail.com>
2022-05-24 10:30:20 +0800

    Updated Plants.jsx

--- src/components/Dashboard/Plants.jsx
+++ src/components/Dashboard/Plants.jsx
@@ -19,25 +19,25 @@ const Plants = ({level}) => {
     case 0:
       return(
       <Container>
-        <PlantCard image= {Kale} title="Kale" desc="O NFTs" value="0.3/eth"/>
-        <PlantCard image = {Tomato} title="Tomato" desc="0 NFTs" value="0.4/eth"/>
-        <PlantCard image = {Cucumber} title="Cucumber" desc="0 NFTs" value="0.5/eth"/>
+        <PlantCard image= {Kale} title="Kale" desc="0 NFTs"/>
+        <PlantCard image = {Tomato} title="Tomato" desc="0 NFTs"/>
+        <PlantCard image = {Cucumber} title="Cucumber" desc="0 NFTs" />
       </Container>
       )
     case 1:
       return (
       <Container>
-      <PlantCard image= {Kale} title="Kale" desc="O NFTs" value="0.3/eth"/>
-      <PlantCard image = {Tomato} title="Tomato" desc="1 NFTs" value="0.4/eth"/>
-      <PlantCard image = {Cucumber} title="Cucumber" desc="3 NFTs" value="0.5/eth"/>
+      <PlantCard image= {Kale} title="Kale" desc="0 NFTs"/>
+      <PlantCard image = {Tomato} title="Tomato" desc="1 NFTs"/>
+      <PlantCard image = {Cucumber} title="Cucumber" desc="3 NFTs"/>
     </Container>
       );
       case 2: 
       return (
         <Container>
-        <PlantCard image= {Kale} title="Kale" desc="3 NFTs" value="0.3/eth"/>
-        <PlantCard image = {Tomato} title="Tomato" desc="O NFTs" value="0.4/eth"/>
-        <PlantCard image = {Cucumber} title="Cucumber" desc="2 NFTs" value="0.5/eth"/>
+        <PlantCard image= {Kale} title="Kale" desc="3 NFTs"/>
+        <PlantCard image = {Tomato} title="Tomato" desc="0 NFTs"/>
+        <PlantCard image = {Cucumber} title="Cucumber" desc="2 NFTs"/>
     </Container>
       )
       default:
