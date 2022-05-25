c252ba09f8d5df9758b1d67bd341ffb33882f694
keng-swee <dreamsblaz@gmail.com>
2022-05-25 19:04:00 +0800

    added axios and post to backend on a button

--- src/components/Dashboard/components/NFTs.jsx
+++ src/components/Dashboard/components/NFTs.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect  } from "react";
 import {Button } from "@mui/material";
+import axios from "axios";
 
 import {
   useMoralis,
   useMoralisWeb3Api,
   useNFTBalances,
+  useNativeTransactions,
 
 } from "react-moralis";
 import "react-tabs/style/react-tabs.css";
 import styled from "styled-components";
 import NFTCard from "./NFTCard";
+
+//CHANGE TO BACKEND SERVER AFTER DEPLOYMENT
+const baseURL = "http://localhost:8080";
+
+
 
 
 const Container = styled.div`
@@ -34,7 +41,9 @@ const NFTs = () => {
     const { user, Moralis, isInitialized } = useMoralis();
 
 
-    const { getNFTBalances, data: NFTbalance, error, isLoading, isFetching } = useNFTBalances();
+    const { getNFTBalances, data: NFTbalance } = useNFTBalances();
+    const { getNativeTransations, data:transList , chainId} = useNativeTransactions();
+
 
 
      //get specific nft detail from OPENSEA (button below, try to make it when it loads or smt i guess)
@@ -76,10 +85,56 @@ const NFTs = () => {
       }
     },[]);
 
+    const postUserDets = async () => {
+        getNativeTransations({ params: { chain: "rinkeby" } });
+        //IDK what you can do with the transactions but you can look at console.log and come up with something creative i think use the gas price and play with it seems to have some potential i guess
+        // or can use transactions to be another set of data to post to backend idk
+
+        console.log(transList);
+        //post user data
+        axios({
+          method: 'post',
+          url: baseURL + '/user',
+          data: {
+            ethAddress: user.attributes.ethAddress,
+            lifetimeEarnings: '4.1298',
+            inventory: null,
+          }
+        });
+        //post NFT data 
+        NFTbalance.result.map(token => axios({
+          method: 'post',
+          url: baseURL + '/' +  user.attributes.ethAddress + '/produce',
+          data: {
+            contractAddress: token.token_address,
+            quantity: token.amount,
+          }
+        }) );
+    };
+
+    const postNFT = async () => {
+        const NFTdata = await Moralis.Plugins.opensea.getAsset({
+        network: "testnet",
+        tokenAddress: "0xBc4595D6d8Cc28C3f611f3B9d778270935e9C8a1",
+        tokenId: "1",
+
+      });
+      axios({
+        method: 'post',
+        url: baseURL + '/user',
+        data: {
+          ethAddress: user.attributes.ethAddress,
+          lifetimeEarnings: '4.1298',
+          inventory: null,
+        }
+      });
+      // console.log(NFTdata);
+    };
+
     
 
     //print out the NFTs under this account
-    console.log(NFTbalance);
+    // console.log(NFTbalance);
 
 
   return (
@@ -88,6 +143,11 @@ const NFTs = () => {
       <Button onClick={getAsset}>Get Asset</Button>
       {/* CLICK TO SELL KALE HARDCODED KALE ADDRESS & USER ACCT TOKEN  INSIDE FUNCTION */}
       <Button onClick={createSellOrder}>Create Sell Order</Button>
+      {/* CLICK TO SELL KALE HARDCODED KALE ADDRESS & USER ACCT TOKEN  INSIDE FUNCTION */}
+      <Button onClick={postUserDets}>Post User Dets</Button>
+      {/* CLICK TO SELL KALE HARDCODED KALE ADDRESS & USER ACCT TOKEN  INSIDE FUNCTION */}
+      <Button onClick={postNFT}>Post KALE NFT dets</Button>
+
 
       {/* { NFTbalance != null  && NFTbalance.result.map((token) => NFTCard( Moralis.Plugins.opensea.getAsset({
           network: "testnet",
